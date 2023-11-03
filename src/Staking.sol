8a256598ad401d218b6d456b0eb0b9ba1026d772
0xxbrain01 <0xxbrain@gmail.com>
2023-11-03 12:27:43 +0700

    add staking

--- src/Staking.sol
+++ src/Staking.sol
@@ -4,8 +4,6 @@ pragma solidity ^0.8.18;
 import "openzeppelin-contracts/contracts/utils/cryptography/ECDSA.sol";
 import "openzeppelin-contracts/contracts/token/ERC20/IERC20.sol";
 
-//import "@forge-std/src/interfaces/IERC20.sol";
-
 contract Staking {
     using ECDSA for bytes32;
 
@@ -33,12 +31,95 @@ contract Staking {
         rewardsToken = IERC20(_rewardToken);
     }
 
-    modifier onlyOwner(){
-      require(msg.sender == owner, "Not Auth");
-      _;
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Not Auth");
+        _;
     }
 
-    modifier updateReward(address _account){
-      rewardPerTokenStored = rewardsToken()
+    modifier updateReward(address _account) {
+        rewardPerTokenStored = rewardPerToken();
+        updateAt = lastTimeRewardApplicable();
+        if (_account != address(0)) {
+            rewards[_account] = earned(_account);
+            userRewardPerTokenPaid[_account] = rewardPerTokenStored;
+        }
+        _;
+    }
+
+    function lastTimeRewardApplicable() public view returns (uint) {
+        return _min(finishAt, block.timestamp);
+    }
+
+    function rewardPerToken() public view returns (uint) {
+        if (totalSupply == 0) {
+            return rewardPerTokenStored;
+        }
+        return
+            rewardPerTokenStored +
+            (rewardRate * (lastTimeRewardApplicable() - updateAt) * 1e18) /
+            totalSupply;
+    }
+
+    function stake(uint _amount) external payable updateReward(msg.sender) {
+        require(_amount > 0, "amount =0");
+        stakingToken.transferFrom(msg.sender, address(this), _amount);
+        balanceOf[msg.sender] += _amount;
+        totalSupply += _amount;
+    }
+
+    function withdraw(uint _amount) external updateReward(msg.sender) {
+        require(_amount > 0, "amount =0");
+        balanceOf[msg.sender] -= _amount;
+        totalSupply -= _amount;
+        stakingToken.transfer(msg.sender, _amount);
+    }
+
+    function check(
+        bytes32 _hash,
+        bytes memory _signature
+    ) public view returns (bool) {
+        return _hash.recover(_signature) == owner;
+    }
+
+    function earned(address _account) public view returns (uint) {
+        return
+            ((balanceOf[_account] *
+                (rewardPerToken() - userRewardPerTokenPaid[_account])) / 1e18) +
+            rewards[_account];
+    }
+
+    function getReward() external updateReward(msg.sender) {
+        uint reward = rewards[msg.sender];
+        if (reward > 0) {
+            rewards[msg.sender] = 0;
+            rewardsToken.transfer(msg.sender, reward);
+        }
+    }
+
+    function setRewardsDuration(uint _duration) external onlyOwner {
+        require(finishAt < block.timestamp, "reward duration not finished");
+        duration = _duration;
+    }
+
+    function notifyRewardAmount(
+        uint _amount
+    ) external onlyOwner updateReward(address(0)) {
+        if (block.timestamp >= finishAt) {
+            rewardRate = _amount / duration;
+        } else {
+            uint remainingRewards = (finishAt - block.timestamp) * rewardRate;
+            rewardRate = (_amount + remainingRewards) / duration;
+        }
+        require(rewardRate > 0, "Reward rate = 0");
+        require(
+            rewardRate * duration <= rewards.balanceOf(address(this)),
+            "reward amount > balance"
+        );
+        finishAt = block.timestamp + duration;
+        updateAt = block.timestamp;
+    }
+
+    function _min(uint x, uint y) private pure returns (uint) {
+        return x <= y ? x : y;
     }
 }
